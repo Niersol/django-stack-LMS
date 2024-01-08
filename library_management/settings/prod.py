3ac62c075036b8f2f8afa9af899b67945556f470
Anas <2122yahooo@gmail.com>
2024-01-09 02:12:44 +0500

    ...

--- library_management/settings/prod.py
+++ library_management/settings/prod.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['digital-lib-f455cebba397.herokuapp.com','*']
+ALLOWED_HOSTS = ['digital-lib-f455cebba397.herokuapp.com','www.niersol.com']
 
 DATABASES = {
     'default': dj_database_url.config()
